e3dd03627f2dc28b022a0450cad2fdd0e5ea4940
Saleh <salehv@gmail.com>
2022-02-20 16:58:52 +0330

    UI Elements added

---
+++ Runtime/Scripts/Pender/UI/UIManager.cs
@@ -0,0 +1,25 @@
+namespace Pender.UI
+{
+    public class UIManager
+    {
+        public static void SetCurrencyCount(int count)
+        {
+            
+        }
+
+        public static void ShowWin()
+        {
+            
+        }
+
+        public static void ShowLose()
+        {
+            
+        }
+
+        public static void ShowSettings()
+        {
+            
+        }
+    }
+}
